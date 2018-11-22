e9ad0101745a8b436b3393d868c77316fc29fb8a
Taomercy <35693926+Taomercy@users.noreply.github.com>
2018-11-22 22:59:13 +0800

    pg math 310

---
+++ MathCaculator/postg_math_310.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from __future__ import division
+from sympy import *
+x, n = symbols('x, n')
+f = x/(1+x)
+print "f1 =", f
+print "f2 =", simplify(f.subs(x, f))
+print "f3 =", simplify(f.subs(x, f).subs(x, f))
+
+fn = x/(n*x+1)
+Sn = integrate(fn, (x, 0, 1))
+print "Sn =", Sn
+print "lim n->oo n*Sn = ", limit(n*Sn, n, oo)
