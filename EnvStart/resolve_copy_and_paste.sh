181fc064085b1635eedd18c2f6fc21377616a3dc
taomercy <taomercy@qq.com>
2018-12-04 19:02:46 -0800

    file struct

--- EnvStart/resolve_copy_and_paste.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#解决复制粘贴失效
-sudo apt-get autoremove open-vm-tools
-sudo apt-get install open-vm-tools-desktop
-reboot
--- EnvStart/resolve_grub_pc_error.sh
+++
@@ -1,4 +0,0 @@
-cd /var/lib/dpkg
-sudo mv info info.bak
-sudo mkdir info
-sudo apt-get install shotwell
