1040ac1eac39c5486f81f127fcccbfebf91adc55
ziwwuex <wei.wu@cienet.com.cn>
2019-07-14 19:42:39 -0700

    animation roll

--- git_push.sh
+++ git_push.sh
@@ -9,7 +9,9 @@ if [[ $? -ne 0 ]];then
     echo "Current path is not a repo path"
     exit 1
 fi
-rm -rf .idea
+find . -name "*.pyc"  | xargs rm -f
+find . -name ".idea" | xargs rm -rf
+git_host=`git config remote.origin.url | awk -F '/' '{print $3}'`
 branch=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
 git diff
 echo "***********************"
@@ -23,4 +25,8 @@ fi
 echo "pushed"
 git add -A
 git commit -m "$commit_msg"
-git push
+if [[ $git_host == "github.com" ]];then
+    git push
+else
+    git push origin HEAD:refs/for/$branch
+fi
