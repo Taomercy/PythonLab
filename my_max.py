fbc2cc9de59685600805937e2b1a8435e474834c
taomercy <taomercy@qq.com>
2021-06-18 10:33:20 +0800

    my_max

--- my_max.py
+++ my_max.py
@@ -1,28 +1,40 @@
 from collections.abc import Iterable
 
 
-def my_max(*args, key=None, default=None):
-    def inner_max(arg):
-        max_value = arg[0]
-        if not key:
-            for i in arg[1:]:
-                if i > max_value:
-                    max_value = i
-        else:
-            for i in arg[1:]:
-                if key(i) >= key(max_value):
-                    max_value = i
-        return max_value
+# def my_max(*args, key=None, default=None):
+#     def inner_max(arg):
+#         max_value = arg[0]
+#         if not key:
+#             for i in arg[1:]:
+#                 if i > max_value:
+#                     max_value = i
+#         else:
+#             for i in arg[1:]:
+#                 if key(i) >= key(max_value):
+#                     max_value = i
+#         return max_value
+#
+#     if len(args) == 1:
+#         assert isinstance(args[0], Iterable)
+#         if len(args[0]) == 0:
+#             return default
+#         return inner_max(args[0])
+#     elif len(args) > 1:
+#         return inner_max(args)
+#     else:
+#         raise Exception('parameter error')
 
-    if len(args) == 1:
-        assert isinstance(args[0], Iterable)
-        if len(args[0]) == 0:
-            return default
-        return inner_max(args[0])
-    elif len(args) > 1:
-        return inner_max(args)
-    else:
-        raise Exception('parameter error')
+
+def my_max(*iterable, key=lambda x: x, default=None):
+    if not iterable or len(iterable) == 0:
+        return default
+    elif len(iterable) == 1 and isinstance(iterable[0], Iterable):
+        iterable = iterable[0]
+    current = None
+    for i in iterable:
+        if current is None or key(i) > key(current):
+            current = i
+    return current
 
 
 a = [1, 3, 2, 5]
