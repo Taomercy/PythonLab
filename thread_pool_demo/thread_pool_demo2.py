a272746736a3aecefda33c8a33e50e5be9f8840d
taomercy <taomercy@qq.com>
2021-12-16 17:44:07 +0800

    map

--- thread_pool_demo/thread_pool_demo2.py
+++ thread_pool_demo/thread_pool_demo2.py
@@ -4,14 +4,15 @@ import time
 import threadpool
 
 
-def sayhello(str):
-    print("Hello", str)
+def sayhello(str, a):
+    print("Hello", str, a)
     time.sleep(2)
 
-name_list =['xiaozi','aa','bb','cc']
+name_list = ['xiaozi','aa','bb','cc']
+params = [([name, "gg"], None) for name in name_list]
 start_time = time.time()
 pool = threadpool.ThreadPool(10)
-requests = threadpool.makeRequests(sayhello, name_list)
+requests = threadpool.makeRequests(sayhello, params)
 [pool.putRequest(req) for req in requests]
 pool.wait()
 print("%d second" % (time.time()-start_time))
