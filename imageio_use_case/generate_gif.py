e850880b501b62794dd39f285756fc614f3a19c1
taomercy <taomercy@qq.com>
2022-10-24 15:58:55 +0800

    prtsc

--- imageio_use_case/generate_gif.py
+++ imageio_use_case/generate_gif.py
@@ -2,22 +2,36 @@
 # -*- coding:utf-8 -*-
 import os
 
-import imageio
+from functools import cmp_to_key
+import imageio.v2 as imageio
 
 
 def create_gif(image_list, gif_name, duration=1):
     frames = []
     for image_name in image_list:
         frames.append(imageio.imread(image_name))
+        print(image_name)
     imageio.mimsave(gif_name, frames, 'GIF', duration=duration)
     return
 
 
+def mycmp(a, b):
+    na = int(os.path.basename(a).split(".")[0])
+    nb = int(os.path.basename(b).split(".")[0])
+    if na > nb:
+        return 1
+    elif na < nb:
+        return -1
+    else:
+        return 0
+
+
 def main():
-    store_path = "source1"
+    store_path = "m1"
     image_list = [os.path.join(store_path, image)for image in os.listdir(store_path)]
-    gif_name = 'source1.gif'
-    duration = 1
+    image_list = sorted(image_list, key=cmp_to_key(mycmp))
+    gif_name = 'm1.gif'
+    duration = 0.1
     create_gif(image_list, gif_name, duration)
 
 
